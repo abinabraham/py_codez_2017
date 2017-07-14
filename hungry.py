7191ef22014e524481ccf71304fcb2f115be245e
Abin Abraham <abinabrahamcs@gmail.com>
2017-07-13 21:41:48 -0700

    Updated with ANgular Basic Code

--- hungry.py
+++ hungry.py
@@ -12,11 +12,26 @@ def arg_dec(operation):
 
     return wrapper
 
+class Sum(object):
+    def __init__(self,a,b):
+        self.a =int(a)
+        self.b=int(b)
 
-@arg_dec
-def sum(a, b):
-    print a + b
-    return a + b
+    @arg_dec
+    def sum(self, a, b):
+        return a+b
 
+class Sub:
+    def __init__(self,a,b):
+        self.a =a
+        self.b=b
 
-sum(1,2)
+    @arg_dec
+    def sub(a, b):
+        print a - b
+        return a - b
+
+s = Sum(1,2)
+
+print "The Sum of is ",s.sum
+
